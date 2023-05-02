2a37ee79b08ff470e2638c4c0f7ede30a06cd0f6
Debatreya Das <116421305+Debatreya@users.noreply.github.com>
2023-05-02 17:12:37 +0530

    Add files via upload

--- root/js/script.js
+++ root/js/script.js
@@ -1,9 +1,17 @@
-const slideImages = document.querySelectorAll('img');
+// Auto Slide Images
+const slideImages = document.querySelectorAll('.slideimg');
 let count = 0
 setInterval(() => {
     document.querySelector(".active").style.animation = "next1 1s ease-in forwards";
     document.querySelector(".active").classList.remove("active");
     slideImages[count].setAttribute("class", "active");
-    document.querySelector(".active").style.animation = "next2 1s ease-in forwards";
-    count = (count+1)%8;
+    document.querySelector(".active").style.animation = "next2 1s ease-out forwards";
+    count = (count+1)%(slideImages.length);
+}, 3000);
+
+// autoslide cards
+slideCards = document.querySelectorAll(".card")
+setInterval(() => {
+    document.querySelector(".activecard").classList.remove("activecard")
+    slideCards[Math.floor(Math.random()*8)].classList.add("activecard")
 }, 3000);
