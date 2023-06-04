976b2e9440a8558a3ab047905e743d693b667e7c
Debatreyta <debatreyadas@gmail.com>
2023-06-04 10:38:57 +0530

    Expressjs and vercel

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static('root'))
+
+// Serve static files from the 'root' folder
+app.use(express.static(path.join(__dirname, 'root')));
+
+// Serve static files from the 'logos' folder
+app.use('/logos', express.static(path.join(__dirname, 'logos')));
+
+// Route for the homepage
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'root', 'index.html'));
+});
+
+app.listen(3000, ()=>{
+    console.log('server is listening to post 300, http://localhost:3000');
+})
